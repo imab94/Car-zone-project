9cdc079eadb02d79a7bba5a1698d2d4fc4d49984
Arun Bhardwaj <bhardwajarun41997@gmail.com>
2023-10-28 19:44:32 +0530

    created pages and navigations navbar

--- carzone/carzone/settings.py
+++ carzone/carzone/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
-    STATIC_DIR,
+    os.path.join(BASE_DIR,'static'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
--- carzone/pages/views.py
+++ carzone/pages/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
     return render(request, 'pages/home.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
